f6efc069b99e58ce83b49c639b3d15a70eae382a
XiangHui556 <2101993@sit.singaporetech.edu.sg>
2023-10-17 09:24:39 +0800

    webhook test

--- api/test.js
+++ api/test.js
@@ -1,8 +1,3 @@
-// This example uses Express to receive webhooks
-// https://stackoverflow.com/questions/27599614/var-express-requireexpress-var-app-express-what-is-express-is-it
-const express = require('express');
-const app = express();
-
 // Instantiating formsg-sdk without parameters default to using the package's
 // production public signing key.
 const formsg = require('@opengovsg/formsg-sdk')();
@@ -17,77 +12,23 @@ const formSecretKey = process.env.FORM_SECRET_KEY;
 
 // Set to true if you need to download and decrypt attachments from submissions
 const HAS_ATTACHMENTS = false;
-console.log('checkpoint 0');
-app.post(
-  '/api/idd_formsg_webhook_email',
-  // Endpoint authentication by verifying signatures
-  function (req, res, next) {
-    console.log('checkpoint 1');
-    try {
-      formsg.webhooks.authenticate(req.get('X-Formsg-Signature'), POST_URI);
-      // Continue processing the POST body
-      return next();
-    } catch (e) {
-      return next();
-      // return res.status(401).send({ message: 'Unauthorized' })
-    }
-  },
-  // Parse JSON from raw request body
-  express.json(),
-  // Decrypt the submission
-  async function (req, res, next) {
-    // If `verifiedContent` is provided in `req.body.data`, the return object
-    // will include a verified key.
+
+module.exports = async (req, res) => {
+  console.log('Starting Genshin Draw Watchlist Get API');
+  console.log(formSecretKey)
+  try {
+    const x = formsg.webhooks.authenticate(
+      req.get('X-Formsg-Signature'),
+      POST_URI
+    );
+    console.log(x);
     const submission = HAS_ATTACHMENTS
       ? await formsg.crypto.decryptWithAttachments(formSecretKey, req.body.data)
       : formsg.crypto.decrypt(formSecretKey, req.body.data);
-
-    // If the decryption failed, submission will be `null`.
-    if (submission) {
-      const nodemailer = require('nodemailer');
-      try {
-        console.log(require.resolve('nodemailer'));
-      } catch (e) {
-        console.error('nodemailer is not found');
-        process.exit(e.code);
-      }
-      console.log('Nodemailer is running');
-      const msg = {
-        from: 'jtcoptimus@gmail.com',
-        to: 'xianghui556@gmail.com',
-        subject: 'Nodemailer Testing',
-        text: 'Testing' + JSON.stringify(submission),
-      };
-      const transporter = nodemailer
-        .createTransport({
-          service: 'gmail',
-          auth: {
-            user: 'jtcoptimus@gmail.com',
-            pass: '0ptimusjtc',
-          },
-          port: 587,
-          host: 'smtp.gmail.com',
-          // secure: true
-        })
-        .sendMail(msg, (err) => {
-          if (err) {
-            return console.log('Error occurs', err);
-          } else {
-            return console.log('Email sent!');
-          }
-        });
-
-      console.log('This is submission' + JSON.stringify(submission));
-      return res.status(200).send({ message: 'See console for submission!' });
-      // Continue processing the submission
-    } else {
-      // console.log('This is submission' + JSON.stringify(submission))
-      return res
-        .status(200)
-        .send({ message: 'Could not decrypt the submission' });
-      // Could not decrypt the submission
-    }
+    console.log(submission);
+    return res.json(false);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return res.status(500).json({ error: 'Internal server error' });
   }
-);
-
-module.exports = app;
+};
